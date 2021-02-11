8f9bc8d24fbaaeecdbe139e11cf56882bfe18f36
aldolopez <aldolopez975@gmail.com>
2021-02-10 18:08:12 -0600

    v3

--- model/usuario/cerrar_sesion.php
+++ model/usuario/cerrar_sesion.php
@@ -4,5 +4,11 @@
     setcookie("usuario","none",time()-1,'/');
     setcookie("password","none",time()-1, '/');
     setcookie("tipo","none",time()-1, '/');
+
+    echo '<script type="text/javascript">';
+    echo 'document.cookie = "usuario=none; max-age= '. time() -1 .'; path=/";';
+    echo 'document.cookie = "usuario=none; max-age= '. time() -1 .'; path=/";';
+    echo "</script>";
+
     header("Location:login");
 ?>
